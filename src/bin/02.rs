82e73311d80e80706a05f19b9579973dd4ed8312
Alex Granatowicz <alex.g@oneupsales.co.uk>
2025-12-04 13:30:57 +0000

    solve(2p2)

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -34,7 +34,31 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut invalid_total: u64 = 0;
+
+    parse_input(input).iter().for_each(|&(start, end)| {
+        invalid_total += (start..(end + 1))
+            .filter(|id| {
+                let string_id = id.to_string();
+                for i in 1..(string_id.len() / 2) + 1 {
+                    // If the id length isn't evenly divisible by the chunk size, skip it
+                    if string_id.len() % i != 0 {
+                        continue;
+                    }
+
+                    let chunk = &string_id[0..i];
+
+                    if chunk.repeat(string_id.len() / chunk.len()) == string_id {
+                        return true;
+                    }
+                }
+                false
+            })
+            .reduce(&mut |a, b| a + b)
+            .unwrap_or(0)
+    });
+
+    Some(invalid_total)
 }
 
 #[cfg(test)]
@@ -50,6 +74,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result.unwrap(), 4174379265);
     }
 }
