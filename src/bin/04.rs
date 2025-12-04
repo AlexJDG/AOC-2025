12d6ce08de4923ae6b4272c774b0626c491e543b
Alex Granatowicz <alex.g@oneupsales.co.uk>
2025-12-04 18:19:47 +0000

    solve(4p2)

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::ops::Sub;
 
 advent_of_code::solution!(4);
 
@@ -29,27 +30,43 @@ const POSITION_OFFSETS: [(i32, i32); 8] = [
     (1, 1),
 ];
 
+pub fn get_accessible_positions(
+    paper_positions: &HashSet<(i32, i32)>,
+) -> impl Iterator<Item = &(i32, i32)> {
+    paper_positions.iter().filter(|(x, y)| {
+        POSITION_OFFSETS
+            .iter()
+            .filter(|(x_offset, y_offset)| paper_positions.contains(&(x + x_offset, y + y_offset)))
+            .count()
+            < 4
+    })
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let paper_positions = parse_input(input);
 
-    Some(
-        paper_positions
-            .iter()
-            .filter(|(x, y)| {
-                POSITION_OFFSETS
-                    .iter()
-                    .filter(|(x_offset, y_offset)| {
-                        paper_positions.contains(&(x + x_offset, y + y_offset))
-                    })
-                    .count()
-                    < 4
-            })
-            .count() as u64,
-    )
+    Some(get_accessible_positions(&paper_positions).count() as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut paper_positions = parse_input(input);
+
+    let mut previous_count: usize = 0;
+    let mut removals: u64 = 0;
+
+    while previous_count != paper_positions.len() {
+        previous_count = paper_positions.len();
+
+        let diff = get_accessible_positions(&paper_positions)
+            .map(|el| *el)
+            .collect::<HashSet<_>>();
+
+        removals += diff.len() as u64;
+
+        paper_positions = paper_positions.sub(&diff);
+    }
+
+    Some(removals)
 }
 
 #[cfg(test)]
@@ -65,6 +82,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(43));
     }
 }
