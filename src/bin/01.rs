642c56a144bafabeef3717c3b9294def782812a8
Alex Granatowicz <alex.g@oneupsales.co.uk>
2025-12-04 13:45:00 +0000

    Simplifying day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,14 +1,17 @@
 advent_of_code::solution!(1);
 
 pub fn parse_input(input: &str) -> Vec<(char, i32)> {
-    input.lines().map(|line| {
-        let (dir, count) = line.split_at(1);
-        (dir.parse::<char>().unwrap(), count.parse::<i32>().unwrap())
-    }).collect()
+    input
+        .lines()
+        .map(|line| {
+            let (dir, count) = line.split_at(1);
+            (dir.parse::<char>().unwrap(), count.parse::<i32>().unwrap())
+        })
+        .collect()
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut dial:i32 = 50;
+    let mut dial: i32 = 50;
     let mut zero_count: u64 = 0;
     parse_input(input).iter().for_each(|&(dir, count)| {
         let mul = if dir == 'L' { -1 } else { 1 };
@@ -28,33 +31,35 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut dial:i32 = 50;
+    let mut dial: i32 = 50;
     let mut zero_count: u64 = 0;
 
-    parse_input(input).iter().for_each(|&(dir, mut dial_turns)| {
-        let mul = if dir == 'L' { -1 } else { 1 };
+    parse_input(input)
+        .iter()
+        .for_each(|&(dir, mut dial_turns)| {
+            let mul = if dir == 'L' { -1 } else { 1 };
 
-        if dial_turns >= 100 {
-            zero_count += (dial_turns / 100) as u64;
-            dial_turns %= 100;
-        }
+            if dial_turns >= 100 {
+                zero_count += (dial_turns / 100) as u64;
+                dial_turns %= 100;
+            }
 
-        let diff = dial_turns * mul;
+            let diff = dial_turns * mul;
 
-        if (dial < 0 && (dial + diff > 0)) || (dial > 0 && (dial + diff < 0) || dial + diff > 100) {
-            zero_count += 1;
-        }
+            if dial > 0 && (dial + diff < 0) || dial + diff > 100 {
+                zero_count += 1;
+            }
 
-        dial = (dial + diff) % 100;
+            dial = (dial + diff) % 100;
 
-        if dial < 0 {
-            dial += 100
-        }
+            if dial < 0 {
+                dial += 100
+            }
 
-        if dial == 0 {
-            zero_count += 1;
-        }
-    });
+            if dial == 0 {
+                zero_count += 1;
+            }
+        });
 
     Some(zero_count)
 }
