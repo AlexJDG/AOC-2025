70177c1b8741f9e93b294e10ee093cc58475f04e
Alex Granatowicz <alex.g@oneupsales.co.uk>
2025-12-04 11:09:10 +0000

    solve(1p2)

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -15,8 +15,6 @@ pub fn part_one(input: &str) -> Option<u64> {
 
         dial = (dial + (count * mul)) % 100;
 
-        dbg!(dial);
-
         if dial < 0 {
             dial += 100
         }
@@ -29,8 +27,36 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(zero_count)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut dial:i32 = 50;
+    let mut zero_count: u64 = 0;
+
+    parse_input(input).iter().for_each(|&(dir, mut dial_turns)| {
+        let mul = if dir == 'L' { -1 } else { 1 };
+
+        if dial_turns >= 100 {
+            zero_count += (dial_turns / 100) as u64;
+            dial_turns %= 100;
+        }
+
+        let diff = dial_turns * mul;
+
+        if (dial < 0 && (dial + diff > 0)) || (dial > 0 && (dial + diff < 0) || dial + diff > 100) {
+            zero_count += 1;
+        }
+
+        dial = (dial + diff) % 100;
+
+        if dial < 0 {
+            dial += 100
+        }
+
+        if dial == 0 {
+            zero_count += 1;
+        }
+    });
+
+    Some(zero_count)
 }
 
 #[cfg(test)]
@@ -46,6 +72,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result.unwrap(), 8u64);
     }
 }
